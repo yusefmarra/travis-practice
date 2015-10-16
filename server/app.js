5029cd0be2c48ac19b27654801c9ba3763e46ca3
yusefmarra <yusef.marra@gmail.com>
2015-10-16 10:44:15 -0600

    blah

--- server/app.js
+++ server/app.js
@@ -5,7 +5,6 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var swig = require('swig');
 
 
 // *** routes *** //
@@ -14,13 +13,6 @@ var routes = require('./routes/index.js');
 
 // *** express instance *** //
 var app = express();
-
-
-// *** view engine *** //
-var swig = new swig.Swig();
-app.engine('html', swig.renderFile);
-app.set('view engine', 'html');
-
 
 // *** static directory *** //
 app.set('views', path.join(__dirname, 'views'));
@@ -53,7 +45,7 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
+    res.json({
       message: err.message,
       error: err
     });
@@ -64,7 +56,7 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error', {
+  res.json({
     message: err.message,
     error: {}
   });
