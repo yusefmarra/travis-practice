90139939f02552b6be2056dbfb24527891bfb58b
yusefmarra <yusef.marra@gmail.com>
2015-10-16 10:35:03 -0600

    First commit

---
+++ test/test.js
@@ -0,0 +1,27 @@
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var server = require('../server/app');
+
+var should = chai.should();
+
+chai.use(chaiHttp);
+
+
+describe('/ route', function() {
+  it('Should return 200', function(done) {
+    chai.request(server)
+      .get('/')
+      .end(function(err, res) {
+        res.should.have.status(200);
+        done();
+      });
+  });
+  it('Should return 404', function(done) {
+    chai.request(server)
+      .get('/')
+      .end(function(err, res) {
+        res.should.have.status(404);
+        done();
+      });
+  });
+});
